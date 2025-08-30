6a7145bd57f376b32485d7ba75d0670c894b4087
dapao669 <jieshou2222@gmail.com>
2025-08-30 16:10:22 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -17,11 +17,11 @@ async fn hello_world() -> &'static str {
 async fn setup_environment() { 
     let env_vars = [
         ("UUID", "66e5c8dd-3176-458e-8fb0-1ed91d2f9602"),
-        ("NEZHA_SERVER", ""),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
-        ("NEZHA_PORT", ""),     // 哪吒v1请留空此变量，哪吒v0的agent端口
-        ("NEZHA_KEY", ""),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
-        ("ARGO_DOMAIN", ""),    // argo固定隧道域名，留空将使用临时隧道
-        ("ARGO_AUTH", ""),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
+        ("NEZHA_SERVER", "nezha.9943826.xyz"),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
+        ("NEZHA_PORT", "443"),     // 哪吒v1请留空此变量，哪吒v0的agent端口
+        ("NEZHA_KEY", "KB9Dc1tcNZjSgkWBae"),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
+        ("ARGO_DOMAIN", "shuttlethree.9943826.xyz"),    // argo固定隧道域名，留空将使用临时隧道
+        ("ARGO_AUTH", "eyJhIjoiYTIyMWM5NDJhZTBjNDg0ZDNhMmZjNTM4NDI5ODMxZDQiLCJ0IjoiY2Y0MGI3YjYtMjJlMi00N2ExLTkxNzEtODc1OTcxNjgwYmI5IiwicyI6Ik9HSmxOMlJtTm1NdFpqZzRNeTAwTlRFMUxUbGxZalV0WmpRNE9HSmxNVGhsTlRNMSJ9"),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
         ("ARGO_PORT", "8080"),  // argo端口，使用固定隧道token，需要在cloudflare后台也设置端口为8080
         ("CFIP", "time.is"),    // 优选域名或优选ip
         ("CFPORT", "443"),      // 优选域名或优选ip对应的端口
